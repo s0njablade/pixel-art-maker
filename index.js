1be527fc63723ce1a5d83a473f9c71ef10f906a9
s0njablade <sonjita.chacon@gmail.com>
2018-09-24 09:40:03 -0600

    playing with js to get the colors to change

--- index.js
+++ index.js
@@ -1,22 +1,42 @@
-document.addEventListener('DOMContentLoaded', function () {
+var changeColor = document.querySelectorAll(".colors")
 
+var currentColor = "white"
 
+var penColor = document.querySelectorAll(".pixel")
 
-    var penColor = 'lavender'
+var paintBox = document.getElementById("art")
 
+// function setPixelColor(event) {
+//     event.target.classList.toggle(changeColor)
+// }
 
+for (let i = 0; i < changeColor.length; i++) {
+    changeColor[i].addEventListener("click", grabColor)
+}
 
-    function setPixelColor() {
-        event.target.classList.toggle(penColor)
+function grabColor(event) {
+    currentColor = event.target.id
+}
 
+for (let i = 0; i < penColor.length; i++) {
+    penColor[i].addEventListener("click", applyColor)
+}
+
+function applyColor(event) {
+    if (event.target.style.backgroundColor && event.target.style.backgroundColor != currentColor) {
+        event.target.style.backgroundColor = currentColor
+    } else {
+        event.target.backgroundColor = currentColor
     }
 
-    var paintBox = document.getElementById('art')
+}
 
-    // var onPixelClick = function () {
-    //     event.target.setPixelColor
-    // }
+paintBox.addEventListener("click", grabColor, applyColor)
 
-    paintBox.addEventListener('click', setPixelColor)
+// var paintBox = document.getElementById('art')
 
-})
+// var onPixelClick = function () {
+//     event.target.setPixelColor
+// }
+
+// paintBox.addEventListener('click', setPixelColor)
